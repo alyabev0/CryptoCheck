5479bbb90664d5bb96f8bc1cffc25a5c2a4e5bd9
alyabev0 <alyabev1998@gmail.com>
2022-05-03 23:18:39 +0300

    added routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-import { Switch, Route, Link } from "react-router-dom"
-import { Layout, Typography, Space } from "antd"
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
 
-function App() {
+import {
+  Navbar,
+  Homepage,
+  Cryptocurrencies,
+  News,
+  CryptoDetails,
+} from "./components";
+import "./App.css";
+
+const App = () => {
   return (
-    <div className="App">
+    <div className="app">
       <div className="navbar">
-        
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+       
         </div>
-        <div className="main">
-
-        </div>
-        <div className="footer">
-
-        </div>
-      
-    </div>
+      </div>
   );
-}
+};
 
 export default App;
