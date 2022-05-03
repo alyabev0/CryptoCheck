4f2b4401f8ffa0d8ec611b81069689866362ce12
alyabev0 <alyabev1998@gmail.com>
2022-05-03 03:00:19 +0300

    added counter and callback props to cryptocurrencies

---
+++ src/components/Cryptocurrencies.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import millify from 'millify';
+import { Link } from 'react-router-dom';
+import { Card, Row, Col, Input } from 'antd';
+
+import { useGetCryptosQuery } from '../services/cryptoApi';
+
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
+return(
+<div></div>
+)
+}
+
+export default Cryptocurrencies;
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -24,7 +24,8 @@ const Homepage = () => {
         <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
         <Col span={12}><Statistic title="Total Market Cap:" value={`$${millify(globalStats.totalMarketCap)}`} /></Col>
         <Col span={12}><Statistic title="Total 24h Volume" value={`$${millify(globalStats.total24hVolume)}`} /></Col>
-
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
+        <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
       </Row>
       <div className="home-heading-container">
         <Title level={2} className="home-title">Top 10 Cryptos In The World</Title>
