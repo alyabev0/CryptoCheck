3338d87b287920af80f2ac31ec03c7a9c07a19d7
alyabev0 <alyabev1998@gmail.com>
2022-05-07 21:09:48 +0300

    added loader

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,20 +1,22 @@
-import React, { useState } from 'react';
-import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
-import moment from 'moment';
+import React, { useState } from "react";
+import { Select, Typography, Row, Col, Card } from "antd";
+import moment from "moment";
 
-import { useGetCryptosQuery } from '../services/cryptoApi';
-import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
-import Loader from './Loader';
+import Loader from "./Loader";
 
-const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
+import { useGetCryptosQuery } from "../services/cryptoApi";
+import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
 
 const { Text, Title } = Typography;
 const { Option } = Select;
 
 const News = ({ simplified }) => {
-  const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
-  const { dataset } = useGetCryptosQuery(100);
-  const { dataset: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12 });
+  const [newsCategory, setNewsCategory] = useState("Cryptocurrency");
+  const { data } = useGetCryptosQuery(100);
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory,
+    count: simplified ? 6 : 18,
+  });
 
   if (!cryptoNews?.value) return <Loader />;
 
@@ -25,31 +27,46 @@ const News = ({ simplified }) => {
           <Select
             showSearch
             className="select-news"
-            placeholder="Select a Crypto"
+            placeholder="Enter your crypto here..."
             optionFilterProp="children"
             onChange={(value) => setNewsCategory(value)}
-            filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+            filterOption={(input, option) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            }
           >
             <Option value="Cryptocurency">Cryptocurrency</Option>
-            {dataset?.data?.coins?.map((currency) => <Option value={currency.name}>{currency.name}</Option>)}
+            {data?.data?.coins?.map((currency) => (
+              <Option value={currency.name}>{currency.name}</Option>
+            ))}
           </Select>
         </Col>
       )}
       {cryptoNews.value.map((news, i) => (
-        <Col xs={12} sm={12} lg={2} key={i}>
+        <Col xs={24} sm={12} lg={8} key={i}>
           <Card hoverable className="news-card">
             <a href={news.url} target="_blank" rel="noreferrer">
               <div className="news-image-container">
-                <Title className="news-title" level={4}>{news.name}</Title>
-                <img src={news?.image?.thumbnail?.contentUrl || demoImage} alt="" />
+                <Title className="news-title" level={5}>
+                  {news.name.length > 100
+                    ? `${news.name.substring(0, 100)}...`
+                    : news.name}
+                </Title>
+                <img src={news?.image?.thumbnail?.contentUrl} alt="" key={i} />
               </div>
-              <p>{news.description.length > 100 ? `${news.description.substring(0, 100)}...` : news.description}</p>
+              <p>
+                {news.description.length > 140
+                  ? `${news.description.substring(0, 140)}...`
+                  : news.description}
+              </p>
               <div className="provider-container">
                 <div>
-                  <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt="" />
-                  <Text className="provider-name">{news.provider[0]?.name}</Text>
+                  <Text className="provider-name">
+                    {news.provider[0]?.name}
+                  </Text>
                 </div>
-                <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
+                <Text>
+                  {moment(news.datePublished).startOf("ss").fromNow()}
+                </Text>
               </div>
             </a>
           </Card>
